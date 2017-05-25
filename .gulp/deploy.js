648b967c9d744f649fbdefa2dd7265c35bc47090
面码酱 <xiyanxiyan10@gmail.com>
2017-05-25 13:48:16 +0800

    gulp added
    1.add gulp master

---
+++ .gulp/deploy.js
@@ -0,0 +1,92 @@
+var gulp = require('gulp'),
+    zip = require('gulp-zip'),
+    gulpRimraf = require("gulp-rimraf"),
+    exit = require('gulp-exit'),
+    mySSH = require('./ssh');
+
+module.exports = function (envCfg, cfg) {
+    var envCfg = envCfg;
+    var cfg = cfg;
+
+    gulp.task('copyWebSiteToDist', function () {
+        var deploySrc = [
+            "*.py",
+            "!**/*.pyc",
+            "iseecore/**/*",
+            "models/**/*",
+            "otherpackage/**/*",
+            "services/**/*",
+            "template/**/*",
+            "translations/**/*",
+            "util/**/*",
+            "views/**/*"
+        ];
+        return gulp.src(deploySrc, {base: './'})
+            .pipe(gulp.dest("./dist"));
+    });
+
+    gulp.task('process:setting', function () {
+        return gulp.src("config/" + envCfg.env + "/*")
+            .pipe(gulp.dest("./dist"));
+    });
+
+    gulp.task('process:dockerfile', function () {
+        return gulp.src([
+            ".docker/Dockerfile." + envCfg.type,
+            ".docker/dockerignore." + envCfg.type], {base: './.docker'})
+            .pipe(gulp.dest("./dist"));
+    });
+
+    gulp.task('zipPublishFile', function () {
+        return gulp.src(["dist/**/*"])
+            .pipe(zip('publish.zip'))
+            .pipe(gulp.dest("./dist"))
+    });
+
+    gulp.task('startDeploy', function () {
+        var logName = "deploy-" + envCfg.type;
+        var tag = "v" + new Date().format("yyyyMMdd-hhmmss");
+        return gulp.src("dist/publish.zip")
+            .pipe(mySSH({
+                servers: [cfg.deployServers[0]],
+                dest: cfg.deployPath + '/publish.zip',
+                shell: ['cd ' + cfg.deployPath,
+                    'shopt -s extglob',
+                    'rm -rf !(logs|node_modules|publish.zip)',
+                    'unzip -o publish.zip',
+                    'chmod u+x -R *',
+                    "rm publish.zip",
+                    "mv Dockerfile." + envCfg.type + " Dockerfile",
+                    "mv dockerignore." + envCfg.type + " .dockerignore",
+                    "sudo docker build -t a80:5000/idealens-" + envCfg.type + ":" + tag + " .",
+                    "sudo docker push a80:5000/idealens-" + envCfg.type + ":" + tag,
+                    "cd ../../k8s/",
+                    "./deploy.sh idealens-" + envCfg.type + " " + tag + "",
+                ],
+                logPath: logName
+            }))
+            .pipe(exit());
+    });
+
+    gulp.task("clean:dist", function (cb) {
+        return gulp.src(envCfg.distDir, {read: false})
+            .pipe(gulpRimraf({force: true}));
+    });
+
+};
+
+Date.prototype.format = function (fmt) { //author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
---
+++ gulpfile.js
@@ -0,0 +1,283 @@
+﻿'use strict'
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
+    cssmin = require('gulp-cssmin'),
+    clean = require('gulp-clean'),
+    path = require("path"),
+    argv = require('yargs').argv,
+    path = require("path"),
+    zip = require('gulp-zip'),
+    gulpRimraf = require("gulp-rimraf"),
+    exit = require('gulp-exit'),
+    git = require('gulp-git'),
+    gulpSequence = require('gulp-sequence'),
+    mySSH = require('./.gulp/ssh'),
+    fs = require('fs'),
+    shell = require('gulp-shell');
+
+var deployConfig = {
+    test: {
+        servers: [{
+            sshConfig: {
+                host: '127.0.0.1',
+                port: 22,
+                username: 'lancelot',
+                password: 'lancelot',
+                readyTimeout: 200000
+            }
+        }],
+        deployPath: "/home/web/dist",
+        deploySrc: [],
+        deployServers: []
+    }
+};
+
+var cfg;
+
+function getEnvConf(type) {
+    var conf = {};
+    conf.env = argv.env || "test";
+    conf.type = type;
+    conf.rootDir = path.normalize(path.join(__dirname, type));
+    conf.distDir = path.normalize(path.join(__dirname, "/dist"));
+    conf.distTempDir = path.normalize(__dirname + "/dist/temp");
+    console.log("env: %s", conf.env);
+    console.log("root dir : %s", conf.rootDir);
+    console.log("dist dir : %s", conf.distDir);
+    return conf;
+}
+
+
+gulp.task('echo', shell.task([
+     'echo hello',
+     'echo word'
+]))
+
+gulp.task('pull', function () {
+    var env = argv.env || "test";
+    var repo = {
+        test: "develop",
+        production: "master"
+    };
+    //
+    git.checkout(repo[env], function (err) {
+        if (err) throw err;
+        git.pull('origin', [repo[env]], function (err) {
+            if (err) throw err;
+            var tag = new Date().format("yyyyMMdd.hhmmss");
+            git.tag(tag, 'Version' + tag, function (err) {
+                if (err) throw err;
+            });
+        });
+    });
+});
+
+
+var STATIC_SRC = 'static/';
+var paths = {
+    allCss: [STATIC_SRC + 'css/*.css', STATIC_SRC + 'css/*/*.css'],
+    scripts: [STATIC_SRC + 'js/*.js', STATIC_SRC + 'js/*/*.js']
+};
+
+gulp.task('static:clean', function () {
+    return gulp.src('dist-static/', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('static:copy', ['static:clean'], function () {
+    return gulp.src('static/**/*')
+        .pipe(gulp.dest('dist-static'));
+});
+
+gulp.task('static:css', function () {
+    gulp.src(paths.allCss)
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist-static/css'));
+});
+
+gulp.task('static:uglify', function (cb) {
+    pump([
+            gulp.src(paths.scripts),
+            uglify(),
+            gulp.dest('dist-static/js')
+        ],
+        cb
+    );
+});
+
+gulp.task('static:zip', function () {
+    return gulp.src(["dist-static/**/*"])
+        .pipe(zip('publish.zip'))
+        .pipe(gulp.dest("./dist-static"))
+});
+
+gulp.task('static:deploy', function () {
+    var env = argv.env || "test";
+    cfg = deployConfig[env];
+    if (!cfg) {
+        console.error("Invalid env !!!");
+    }
+
+    cfg.deploySrc = ["dist-static/**"];
+    cfg.deployPath = cfg.deployPath + "/static";
+    cfg.deployServers = cfg.servers;
+
+    var envCfg = getEnvConf("static");
+    var logName = "deploy-" + envCfg.type;
+    gulp.src("dist-static/publish.zip")
+        .pipe(mySSH({
+            servers: [cfg.deployServers[0]],
+            dest: cfg.deployPath + '/publish.zip',
+            shell: ['cd ' + cfg.deployPath,
+                'shopt -s extglob',
+                'sudo rm -rf !(logs|node_modules|publish.zip)',
+                'unzip -o publish.zip',
+                "rm publish.zip"
+            ],
+            logPath: logName
+        }))
+        .pipe(exit());
+});
+
+gulp.task('idealens-static', function () {
+    gulpSequence('static:copy', 'static:css', 'static:uglify', 'static:zip',
+        'static:deploy',
+        function () {
+            console.log("***** Deploy Finished！！！！");
+            process.exit(0);
+        }
+    );
+});
+
+gulp.task('idealens-main', function (cb) {
+    var env = argv.env || "test";
+    cfg = deployConfig[env];
+    if (!cfg) {
+        console.error("Invalid env !!!");
+    }
+    cfg.deploySrc = ["dist/**"];
+    cfg.deployPath = cfg.deployPath + "/app";
+    cfg.deployServers = cfg.servers;
+
+    var envCfg = getEnvConf("main");
+    require("./.gulp/deploy.js")(envCfg, cfg);
+
+    gulpSequence("clean:dist", "copyWebSiteToDist",
+        "process:setting", "process:dockerfile", "zipPublishFile",
+        "startDeploy",
+        function () {
+            console.log("***** Deploy Finished！！！！");
+            process.exit(0);
+        });
+});
+
+gulp.task('idealens-sendemail', function (cb) {
+    var env = argv.env || "test";
+    cfg = deployConfig[env];
+    if (!cfg) {
+        console.error("Invalid env !!!");
+    }
+    cfg.deploySrc = ["dist/**"];
+    cfg.deployPath = cfg.deployPath + "/app";
+    cfg.deployServers = cfg.servers;
+
+    var envCfg = getEnvConf("sendemail");
+    require("./.gulp/deploy.js")(envCfg, cfg);
+
+    gulpSequence("clean:dist", "copyWebSiteToDist",
+        "process:setting", "process:dockerfile", "zipPublishFile",
+        "startDeploy",
+        function () {
+            console.log("***** Deploy Finished！！！！");
+            process.exit(0);
+        });
+});
+
+gulp.task('idealens-sendsms', function (cb) {
+    var env = argv.env || "test";
+    cfg = deployConfig[env];
+    if (!cfg) {
+        console.error("Invalid env !!!");
+    }
+    cfg.deploySrc = ["dist/**"];
+    cfg.deployPath = cfg.deployPath + "/app";
+    cfg.deployServers = cfg.servers;
+
+    var envCfg = getEnvConf("sendsms");
+    require("./.gulp/deploy.js")(envCfg, cfg);
+
+    gulpSequence("clean:dist", "copyWebSiteToDist",
+        "process:setting", "process:dockerfile", "zipPublishFile",
+        "startDeploy",
+        function () {
+            console.log("***** Deploy Finished！！！！");
+            process.exit(0);
+        });
+});
+
+gulp.task('idealens-sendcdn', function (cb) {
+    var env = argv.env || "test";
+    cfg = deployConfig[env];
+    if (!cfg) {
+        console.error("Invalid env !!!");
+    }
+    cfg.deploySrc = ["dist/**"];
+    cfg.deployPath = cfg.deployPath + "/app";
+    cfg.deployServers = cfg.servers;
+
+    var envCfg = getEnvConf("sendcdn");
+    require("./.gulp/deploy.js")(envCfg, cfg);
+
+    gulpSequence("clean:dist", "copyWebSiteToDist",
+        "process:setting", "process:dockerfile", "zipPublishFile",
+        "startDeploy",
+        function () {
+            console.log("***** Deploy Finished！！！！");
+            process.exit(0);
+        });
+});
+
+gulp.task('idealens-checkpay', function (cb) {
+    var env = argv.env || "test";
+    cfg = deployConfig[env];
+    if (!cfg) {
+        console.error("Invalid env !!!");
+    }
+    cfg.deploySrc = ["dist/**"];
+    cfg.deployPath = cfg.deployPath + "/app";
+    cfg.deployServers = cfg.servers;
+
+    var envCfg = getEnvConf("checkpay");
+    require("./.gulp/deploy.js")(envCfg, cfg);
+
+    gulpSequence("clean:dist", "copyWebSiteToDist",
+        "process:setting", "process:dockerfile", "zipPublishFile",
+        "startDeploy",
+        function () {
+            console.log("***** Deploy Finished！！！！");
+            process.exit(0);
+        });
+});
+
+gulp.task('idealens-scheduler-bill', function (cb) {
+    var env = argv.env || "test";
+    cfg = deployConfig[env];
+    if (!cfg) {
+        console.error("Invalid env !!!");
+    }
+    cfg.deploySrc = ["dist/**"];
+    cfg.deployPath = cfg.deployPath + "/app";
+    cfg.deployServers = cfg.servers;
+
+    var envCfg = getEnvConf("scheduler-bill");
+    require("./.gulp/deploy.js")(envCfg, cfg);
+
+    gulpSequence("clean:dist", "copyWebSiteToDist",
+        "process:setting", "process:dockerfile", "zipPublishFile",
+        "startDeploy",
+        function () {
+            console.log("***** Deploy Finished！！！！");
+            process.exit(0);
+        });
+});
