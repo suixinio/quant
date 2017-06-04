60a96d792d56bde1c407038b0feb1041ce62d138
面码酱 <xiyanxiyan10@gmail.com>
2017-06-04 12:23:27 +0800

    1.dynamic params func support
    1.dynamic params func support

--- tools/demo.js
+++ tools/demo.js
@@ -1,6 +1,27 @@
 var Interval = 100
 var stockType = "LTC/CNY"
 
+/**
+ * @brief dynamic param support funcion
+ * @param [func] fn callback function
+ * @param [...] args params
+ */
+function dynCallback(fn, args){
+    var result
+    G.Log(args);
+    try{
+        result = fn.apply(this, args);
+    }catch(e){
+        throw new Error(e.message + "->" + "dynCallback");
+    }
+    return result
+}
+
+/**
+ * @brief ajust number by floor
+ * @param [float] v float number
+ *
+ */
 function adjustFloat(v) {
     return Math.floor(v*1000)/1000;
 }
@@ -10,46 +31,62 @@ function adjustFloat(v) {
  *
  */
 function CancelPendingOrders(stockType) {
-    while (true) {
-        while(!(orders = E.GetOrders(stockType))){}
+    try{
+        while (true) {
+            orders = pendingAction(E.GetOrders, -1, [stockType]);
+            
+            if (orders.length == 0) {
+                return;
+            }
 
-        if (orders.length == 0) {
-            return;
+            for (var i = 0; i < orders.length; i++) {
+                pendingAction(E.CancelOrder, -1, [orders[i]]);
+            }
         }
 
-        for (var i = 0; i < orders.length; i++) {
-            while(!E.CancelOrder(orders[i])){}
-        }
+    }catch(e){
+        throw new Error(e.message + "->" + "CancelPendingOrders");
     }
 }
 
 /**
  *  @brief pending action
- *  @param [int] count >0 ? try count : always pending 
+ *  @param [int] count >=0 ? try count : always pending 
+ *  @param [...] args params
  *
  */
-function pendingAction(func, count) {
+function pendingAction(func, count, args) {
     var result;
     var i = 0;
-    if(count >= 0){
-        while (!(result = func())) {
-            Sleep(Interval);
-            if(i >= count){
-                break;
-            }else{
-                i++;
-            }
-        }
 
-    }else{
-        while (!(result = func())) {
-            Sleep(Interval);
-        }
+    try{
+            while (!(result = dynCallback(func, args))) {
+                Sleep(Interval);
+            
+                //always try if count < 0
+                if(count < 0){
+                    continue;
+                }
+
+                if(i >= count){
+                    break;
+                }else{
+                    i++;
+                }
+            }
+
+    }catch(e){
+        throw new Error(e.message + "->" + "CancelPendingOrders");
     }
     return result;
 }
 
 function main() {
-    G.Log(pendingAction(E.GetAccount, 0));
-    CancelPendingOrders(stockType);
+
+    try{ 
+        G.Log(pendingAction(E.GetAccount, 0));
+        CancelPendingOrders(stockType);
+    }catch(e){
+        throw new Error(e.message);
+    }
 }
